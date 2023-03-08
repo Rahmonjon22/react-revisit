b156be68bb761aedb026d0b5af9bda43930f0279
Rahmonjon22 <rahmonjon2@yahoo.com>
2023-03-08 07:53:26 +0100

    tutorials

---
+++ src/tutorials/tutorials.js
@@ -0,0 +1,4 @@
+export { default as Props } from './props/Props';
+export { default as Redux } from './redux/Redux';
+export { default as UseEffect } from './useEffect/UseEffect';
+export { default as UseState } from './useState/UseState';
